c449c2292c92405f710387f1f8a9cd583b394e8a
PahJustino <paolajustino.forti@gmail.com>
2024-05-28 13:31:18 -0300

    Projetos

---
+++ tipo-variaveis/src/caracteristicas_modelos.java
@@ -0,0 +1,8 @@
+public class caracteristicas_modelos {
+
+    public static void main(String[] args) {
+        Scanner terminal(System.in);
+        System.out.println("Digite a caracteristica do modelo:");
+        int informacao = terminal.??;        
+    }
+}
