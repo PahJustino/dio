5440496175b8bdf9fbbb78c54b6355ab0eb443c9
PahJustino <143464805+PahJustino@users.noreply.github.com>
2024-06-12 13:10:24 -0300

    ProjetoIphone

---
+++ ProjetoIphone/iPhone.java
@@ -0,0 +1,35 @@
+package iphone;
+
+public class iPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+ //aparelho telefonico
+    
+    public void ligar(String numero) {
+    }
+
+    public void atender(String numero) {
+    }
+
+    public void iniciarSMS(String mensagem) {
+    }
+
+// NavegadorInternet
+    public void exibirPagina(String url) {
+    }
+
+   public void adicionarAba(String url) {
+    }
+
+    public void atualizarPagina() {
+    }
+
+// ReprodutorMusical
+    public void tocar() {
+    }
+
+    public void pausar() {
+   }
+
+    public void selecionarMusica() {
+    }
+}
